be4094328ef7cc97d6eee6d4fb68a3dbf287293a
Nemchann <bayaka79@mail.ru>
2026-04-04 10:09:51 +0300

    some changes in AppConfig.java

--- codes_for_Java/src/com/nemchann/training/app/AppConfig.java
+++ codes_for_Java/src/com/nemchann/training/app/AppConfig.java
@@ -14,6 +14,7 @@ public class AppConfig {
         private static final AppConfig INSTANCE = new AppConfig();
     }
 
+
     public static AppConfig getInstance(){
         return Holder.INSTANCE;
     }
